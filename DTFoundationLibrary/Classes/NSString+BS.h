0bbef57753f000d32e66e3c1e587d173b5175ead
ryan <sunzhongyuan@datangwealht.com>
2018-08-18 18:07:40 +0800

    初始化项目.

---
+++ DTFoundationLibrary/Classes/NSString+BS.h
@@ -0,0 +1,12 @@
+//
+//  NSString+BS.h
+//  DTFoundationLibrary
+//
+//  Created by ryan on 2018/8/18.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (BS)
+
+@end
---
+++ DTFoundationLibrary/Classes/NSString+BS.m
@@ -0,0 +1,12 @@
+//
+//  NSString+BS.m
+//  DTFoundationLibrary
+//
+//  Created by ryan on 2018/8/18.
+//
+
+#import "NSString+BS.h"
+
+@implementation NSString (BS)
+
+@end
